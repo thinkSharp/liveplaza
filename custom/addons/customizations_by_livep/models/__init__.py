345a203cd49c7601aeadaeec1301eeb9d1edf2ca
Pyae Phyo Kyaw <69474976+pyaephyokyaw15@users.noreply.github.com>
2022-02-13 21:37:44 +0630

    Solve QA Fail

--- custom/addons/customizations_by_livep/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_partner
-from . import res_country
-from . import website
-from . import delivery_carrier
-from . import sale
-from . import seller_live_stream_inherit
--- custom/addons/customizations_by_livep/models/seller_live_stream_inherit.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
-
-
-class SellerLiveStream(models.Model):
-    _inherit = 'seller.live.stream'
-
-    host = fields.Selection(
-        [('youtube', 'YouTube'), ('facebook', 'Facebook')], string='Host', store=True, default='youtube')
