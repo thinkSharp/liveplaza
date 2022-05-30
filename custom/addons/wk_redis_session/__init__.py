dc1289c7abb539ca5a0c14b091a7e37f7241a4eb
Sughan Richardson Samson Babuji <sughanrichardson1996@gmail.com>
2022-05-30 01:18:50 -0400

    new addon

---
+++ custom/addons/wk_redis_session/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+#
+#    Copyright (c) 2015-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+#
+#################################################################################
+# from . import controllers
+
+from . import http_patch
+from . import models
