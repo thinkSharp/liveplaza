d217e822fb35968caace1b44ed1da02575382bd9
pyaephyokyaw15 <pyaekyaw1571999@gmail.com>
2022-06-16 13:04:02 +0630

    Update Delete features

--- custom/addons/service_product/models/product.py
+++ custom/addons/service_product/models/product.py
@@ -5,3 +5,16 @@ class ProductInherit(models.Model):
     _inherit = 'product.template'
 
     is_service = fields.Boolean(string="Service Type", default=False)
+
+    def delete_discard_products(self):
+        pending_products = self.env["product.template"].search([('status', '=', "draft"), ('type', '=', "product")])
+        print("Pending Products..........", pending_products)
+        for pending_product in pending_products:
+            if not self.env['service.request.product'].search([('product_tmpl_id', '=', pending_product.id)]):
+                products = self.env['product.product'].search([('product_tmpl_id', '=', pending_product.id)])
+
+                for product in products:
+                    stock_quants = self.env['stock.quant'].search([('product_id', '=', product.id)])
+                    for stock_quant in stock_quants:
+                        stock_quant.unlink()
+                    product.unlink()
