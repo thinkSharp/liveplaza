4430af708ebf0782ec0377942c3a78a77fd290ac
Min Hein Khant <minheinkhant.atwork@outlook.com>
2022-07-26 14:17:41 +0630

    empty theme added for livep ui

---
+++ custom/themes/theme_livep/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Theme by livep",
+    'description': "",
+    'version': '1.0',
+    'author': "Min Hein Khant",
+    'category': 'Theme/eCommerce',
+    'depends': [],
+    'data': [
+    ]
+}
