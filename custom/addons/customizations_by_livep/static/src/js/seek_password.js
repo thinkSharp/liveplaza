d618e8097347b4e3003994032cf10e58d2e71794
Hein-Thant-Soe <heinthantsoe46@gmail.com>
2022-08-08 20:32:35 +0630

    Changes to be committed: 	modified:   custom/addons/customizations_by_livep/static/src/css/style.css 	modified:   custom/addons/customizations_by_livep/static/src/js/seek_password.js 	modified:   custom/addons/customizations_by_livep/views/signup_form_inherit.xml

--- custom/addons/customizations_by_livep/static/src/js/seek_password.js
+++ custom/addons/customizations_by_livep/static/src/js/seek_password.js
@@ -1,22 +1,47 @@
 
 
-$(document).ready(function(){
- $("#seekPassword").click(function(){
-    var seekPassword = document.querySelector("#seekPassword");
-    var password = document.querySelector("#password");
-    var type = password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
-    // toggle the icon
-    //seekPassword.classList.toggle("fa-eye");
-    $('#seekPassword').toggleClass("fa-eye-slash fa-eye");
+// $(document).ready(function(){
+//  $("#seekPassword").click(function(){
+//     var seekPassword = document.querySelector("#seekPassword");
+//     var password = document.querySelector("#password");
+//     var type = password.getAttribute("type") === "password" ? "text" : "password";
+//     password.setAttribute("type", type);
+//     // toggle the icon
+//     //seekPassword.classList.toggle("fa-eye");
+//     $('#seekPassword').toggleClass("fa-eye-slash fa-eye");
 
-    // prevent form submit
-    const form = document.querySelector("form");
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-    });
- });
-});
+//     // prevent form submit
+//     const form = document.querySelector("form");
+//     form.addEventListener('submit', function (e) {
+//         e.preventDefault();
+//     });
+//  });
+// });
 
+// $(document).ready(function() {
 
+//     $("#seekPassword").click(function() {
+  
+//       var className = $("#icon").attr('class');
+//       className = className.indexOf('slash') !== -1 ? 'fa fa-eye' : 'fa fa-eye-slash'
+  
+//       $("#icon").attr('class', className);
+//       var input = $("#pass");
+  
+//       if (input.attr("type") == "text") {
+//         input.attr("type", "password");
+//       } else {
+//         input.attr("type", "text");
+//       }
+//     });
+  
+//   });
 
+  function mouseoverPass(obj) {
+    var obj = document.getElementById('password');
+    obj.type = "text";
+  }
+  function mouseoutPass(obj) {
+    var obj = document.getElementById('password');
+    obj.type = "password";
+  }
