701b574aa5c8363825de490b3494b13fa0b7739b
Mang Suan <mangsuan09@gmail.com>
2022-10-07 16:17:49 +0630

    Update amount_delivery for sol_cancel status
    Update amount_delivery for sol_cancel status

--- custom/addons/do_customization/models/sale.py
+++ custom/addons/do_customization/models/sale.py
@@ -73,21 +73,6 @@ class SaleOrder(models.Model):
         ('standard', 'Standard'),
         ('express', 'Express')
     ], default='standard')
-
-    amount_delivery = fields.Monetary(
-        compute='_compute_amount_delivery',
-        string='Delivery Amount',
-        help="The amount without tax.", store=True, tracking=True)
-
-    @api.depends('order_line.price_unit', 'order_line.tax_id', 'order_line.discount', 'order_line.product_uom_qty')
-    def _compute_amount_delivery(self):
-        for order in self:
-            sol_deli_state = self.env['sale.order.line'].search([('order_id','=',self.id), ('is_delivery' , '=' , True), ('sol_state' , '!=' , 'cancel')])                
-            if sol_deli_state:
-                if self.env.user.has_group('account.group_show_line_subtotals_tax_excluded'):
-                    order.amount_delivery = sum(order.order_line.filtered('is_delivery').mapped('price_subtotal'))
-                else:
-                    order.amount_delivery = sum(order.order_line.filtered('is_delivery').mapped('price_total'))
                     
     @api.model
     def _compute_sol_page_break(self, sol_per_page):
@@ -581,6 +566,7 @@ class SaleOrderLine(models.Model):
                         'amount_untaxed': sol_data.order_id.pricelist_id.currency_id.round(amount_untaxed),
                         'amount_tax': sol_data.order_id.pricelist_id.currency_id.round(amount_tax),
                         'amount_total': amount_untaxed + amount_tax,
+                        'amount_delivery': 0.0,
                     })
 
 
