fcbd5823ef4ec6300c8d88d65bccfc143f9a97a8
Mang Suan <mangsuan09@gmail.com>
2022-10-26 16:23:26 +0630

    pricelist_for_booking_product
    pricelist_for_booking_product

---
+++ custom/addons/livep_extra/models/product.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import ValidationError, UserError
+from odoo.addons.http_routing.models.ir_http import slug
+from odoo.addons.website.models import ir_http
+from odoo.tools.translate import html_translate
+from odoo.osv import expression
+from odoo.http import request
+
+import logging
+import re
+
+from odoo.tools import float_compare
+
+_logger = logging.getLogger(__name__)
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    def _get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False, parent_combination=False, only_template=False):
+        """Override for website, where we want to:
+            - take the website pricelist if no pricelist is set
+            - apply the b2b/b2c setting to the result
+
+        This will work when adding website_id to the context, which is done
+        automatically when called from routes with website=True.
+        """
+        self.ensure_one()
+
+        current_website = False
+
+        if self.env.context.get('website_id'):
+            current_website = self.env['website'].get_current_website()
+            if not pricelist:
+                pricelist = current_website.get_current_pricelist()
+        order = request.website.sale_get_order()
+        combination_info = super(ProductTemplate, self)._get_combination_info(
+            combination=combination, product_id=product_id, add_qty=add_qty, pricelist=pricelist,
+            parent_combination=parent_combination, only_template=only_template)
+
+        if self.env.context.get('website_id'):
+            partner = self.env.user.partner_id
+            company_id = current_website.company_id
+            product = self.env['product.product'].browse(combination_info['product_id']) or self
+
+            tax_display = self.env.user.has_group('account.group_show_line_subtotals_tax_excluded') and 'total_excluded' or 'total_included'
+            taxes = partner.property_account_position_id.map_tax(product.sudo().taxes_id.filtered(lambda x: x.company_id == company_id), product, partner)
+
+            # The list_price is always the price of one.
+            quantity_1 = 1
+            price = taxes.compute_all(combination_info['price'], pricelist.currency_id, quantity_1, product, partner)[tax_display]
+            if pricelist.discount_policy == 'without_discount':
+                if self.is_booking_type == True:
+                    if order.website_order_line:
+                        for or_line in order.website_order_line:
+                            if or_line.product_id.product_tmpl_id.id == self.id:
+                               list_price = or_line.booking_slot_id.price
+                    else:
+                        list_price = sorted(self.booking_day_slot_ids.mapped('booking_slots_ids.price'))       
+                        if list_price:
+                            list_price = list_price[0]       
+                    
+                else:
+                    list_price = taxes.compute_all(combination_info['list_price'], pricelist.currency_id, quantity_1, product, partner)[tax_display]
+            else:
+                list_price = price
+            has_discounted_price = pricelist.currency_id.compare_amounts(list_price, price) == 1
+
+            combination_info.update(
+                price=price,
+                list_price=list_price,
+                has_discounted_price=has_discounted_price,
+            )
+
+        return combination_info
+    
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    @api.depends_context('pricelist', 'partner', 'quantity', 'uom', 'date', 'no_variant_attributes_price_extra')
+    def compute_product_price_onwords(self):
+        prices = {}
+        pricelist_id_or_name = self._context.get('pricelist')
+        if pricelist_id_or_name:
+            pricelist = None
+            partner = self.env.context.get('partner', False)
+            quantity = self.env.context.get('quantity', 1.0)
+
+            # Support context pricelists specified as list, display_name or ID for compatibility
+            if isinstance(pricelist_id_or_name, list):
+                pricelist_id_or_name = pricelist_id_or_name[0]
+            if isinstance(pricelist_id_or_name, str):
+                pricelist_name_search = self.env['product.pricelist'].name_search(pricelist_id_or_name, operator='=', limit=1)
+                if pricelist_name_search:
+                    pricelist = self.env['product.pricelist'].browse([pricelist_name_search[0][0]])
+            elif isinstance(pricelist_id_or_name, int):
+                pricelist = self.env['product.pricelist'].browse(pricelist_id_or_name)
+
+            if pricelist:
+                quantities = [quantity] * len(self)
+                partners = [partner] * len(self)
+                prices = pricelist.get_products_price(self, quantities, partners)
+
+        #for product in self:
+        #    product.price = prices.get(product.id, 0.0)
+        return prices
+            
